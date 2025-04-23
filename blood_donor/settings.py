17be7006f3f2106e88b2859cd0305c02693230a2
Baranichandran S <85553313+BARANICHANDRAN@users.noreply.github.com>
2025-04-23 16:05:26 +0530

    Update settings.py

--- blood_donor/settings.py
+++ blood_donor/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dlsghw*u_dncp&i9v!)4ieq2n)05!xomql@u6j78z%qwg4le7u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
