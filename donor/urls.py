c509effad81e2430903b4b0ed0cdfa6430fc475d
Baranichandran S <85553313+BARANICHANDRAN@users.noreply.github.com>
2025-04-23 21:57:29 +0530

    Update urls.py

--- donor/urls.py
+++ donor/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('donor_list/', views.donor_list, name='donor_list'),
+    path('view/', views.view_donor, name='view_donor'),
     path('add/', views.add_donor, name='add_donor'),
     path('edit/<str:contact>/', views.edit_donor, name='edit_donor'),
-    path('', views.donor_list, name='donor_list'),
 ]
