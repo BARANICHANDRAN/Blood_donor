ed452f2d11ba5fa35469011bc08c4211ed87180b
Baranichandran S <85553313+BARANICHANDRAN@users.noreply.github.com>
2025-04-25 16:21:54 +0530

    Add files via upload

---
+++ donor/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('donor_list/', views.donor_list, name='donor_list'),
+    path('view/', views.view_donor, name='view_donor'),
+    path('add/', views.add_donor, name='add_donor'),
+    path('edit/<str:contact>/', views.edit_donor, name='edit_donor'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
